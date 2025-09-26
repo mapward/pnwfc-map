39a870a44e0c490f20c5045ac14114eac069bd13
mapward <greenreign+mapward@gmail.com>
2025-09-26 14:27:54 -0700

    locations: map allRecords to fields data

--- api/locations.js
+++ api/locations.js
@@ -34,7 +34,7 @@ export default async function handler(req, res) {
       offset = data.offset;
     } while (offset);
 
-    res.status(200).json(allRecords);
+    res.status(200).json(allRecords.map(x => { ...x.fields });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Failed to fetch Airtable data" });
