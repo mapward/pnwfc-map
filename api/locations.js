9bff658a03feba750f558744f660f9d6606c281d
mapward <greenreign+mapward@gmail.com>
2025-09-26 14:32:11 -0700

    locations: fix typo

--- api/locations.js
+++ api/locations.js
@@ -36,7 +36,7 @@ export default async function handler(req, res) {
 
     res.status(200).json(allRecords.map(x => { 
       return { ...x.fields }
-    });
+    }));
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Failed to fetch Airtable data" });
