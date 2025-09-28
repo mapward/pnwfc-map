853ecfb3e1bcc3879368b639a38c6287d318b41e
mapward <mapward@users.noreply.github.com>
2025-09-28 14:00:43 -0700

    Refactor: Extract Map component
    Extract a map from the App monolith,
    turning it into a component that takes
    data sources as a prop.

--- www/app.vue
+++ www/app.vue
@@ -1,122 +1,95 @@
 <template>
-	<div>
-		<div id="map" style="height:600px;"></div>
-
-		<div v-cloak id="layer-select-overlay">
-			<div v-for="source in sources">
-				<label>
-					<input type="checkbox" v-model="source.isVisible"/>
-					{{ source.label }}
-				</label>			
-			</div>
+	<Map
+		:isDev 
+		:sources
+		@select-location="locationSelected"
+	>
+		<h2>{{ name }}</h2>
+		<div>
+			<a :href="website" target="_blank">{{ website }}</a>
 		</div>
 
-		<div v-cloak class="map-overlay right" :class="{ show: location, hide: !isInfoWindowVisible }">
-			<div class="map-overlay-border">
-				<svg class="border-svg">
-					<rect x="10" y="10" width="96%" height="96%" fill="none" stroke="currentColor" stroke-width="4" stroke-dasharray="20 12.36" vector-effect="non-scaling-stroke" />
-				</svg>
-				<div v-if="location" class="map-overlay-inner">
-					<!-- X close button from HeroIcons -->
-					<svg id="close-btn" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6" @click="hideInfoWindow">
-						<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-					</svg>
+		<section style="margin-top: 1.5rem">
+			<template v-if="isMember">
+				<template v-if="producerType && isProducer()">
+					<h3>Producer Type</h3>
+					<div>{{ producerType }}</div>
+				</template>
 
-					<h2>{{ name }}</h2>
+				<template v-if="profileUrl">
+					<h3>
+						<a :href="profileUrl" target="_blank">
+							Member Profile
+						</a>
+					</h3>
+				</template>
+				
+				<template v-if="socialUrl">
+					<h3>Social Media</h3>
 					<div>
-						<a :href="website" target="_blank">{{ website }}</a>
+						<a :href="socialUrl" target="_blank">
+							{{ socialUrl }}
+						</a>
 					</div>
+				</template>
+				
+				<template v-if="sell">
+					<h3>Available Products</h3>
+					<div>{{ sell }}</div>
+				</template>
+										
+				<template v-if="education">
+					<h3>Educational Opportunities</h3>
+					<div>{{ education }}</div>
+				</template>
+				
+				<template v-if="classUrl">
+					<h3>Classes</h3>
+					<div>
+						<a :href="classUrl" target="_blank">{{ classUrl }}</a>
+					</div>
+				</template>
 
-					<section style="margin-top: 1.5rem">
-						<template v-if="isMember">
-							<template v-if="producerType && isProducer()">
-								<h3>Producer Type</h3>
-								<div>{{ producerType }}</div>
-							</template>
+				<template v-if="retailType">
+					<h3>Retailer Type</h3>
+					<div>{{ retailType }}</div>
+				</template>
 
-							<template v-if="profileUrl">
-								<h3>
-									<a :href="profileUrl" target="_blank">
-										Member Profile
-									</a>
-								</h3>
-							</template>
-							
-							<template v-if="socialUrl">
-								<h3>Social Media</h3>
-								<div>
-									<a :href="socialUrl" target="_blank">
-										{{ socialUrl }}
-									</a>
-								</div>
-							</template>
-							
-							<template v-if="sell">
-								<h3>Available Products</h3>
-								<div>{{ sell }}</div>
-							</template>
-													
-							<template v-if="education">
-								<h3>Educational Opportunities</h3>
-								<div>{{ education }}</div>
-							</template>
-							
-							<template v-if="classUrl">
-								<h3>Classes</h3>
-								<div>
-									<a :href="classUrl" target="_blank">{{ classUrl }}</a>
-								</div>
-							</template>
+				<template v-if="learningType">
+					<h3>Learning Type</h3>
+					<div>{{ learningType }}</div>
+				</template>
 
-							<template v-if="retailType">
-								<h3>Retailer Type</h3>
-								<div>{{ retailType }}</div>
-							</template>
-
-							<template v-if="learningType">
-								<h3>Learning Type</h3>
-								<div>{{ learningType }}</div>
-							</template>
-
-							<template v-if="producerType">
-								<h3>Producer Type</h3>
-								<div>{{ producerType }}</div>
-							</template>
-							
-							<template v-if="products">
-								<h3>Products</h3>
-								<div>{{ products }}</div>
-							</template>
-								
-							<template v-if="sourcing">
-								<h3>Sourcing Information</h3>
-								<div>{{ sourcing }}</div>
-							</template>
-						</template>			
-						
-						<h3>Location</h3>
-						<div>{{ address }}</div>
-					</section>
-				</div>
-			</div>
-		</div>
-	</div>
+				<template v-if="producerType">
+					<h3>Producer Type</h3>
+					<div>{{ producerType }}</div>
+				</template>
+				
+				<template v-if="products">
+					<h3>Products</h3>
+					<div>{{ products }}</div>
+				</template>
+					
+				<template v-if="sourcing">
+					<h3>Sourcing Information</h3>
+					<div>{{ sourcing }}</div>
+				</template>
+			</template>			
+			
+			<h3>Location</h3>
+			<div>{{ address }}</div>
+		</section>
+	</Map>
 </template>
 
 <script setup>
-// Vue app
-import { createApp, computed, onMounted, reactive, ref, watchEffect } from "vue";
+import { createApp, computed, reactive, ref } from "vue";
+import Map from './map.vue';
 
 // set this to true for easier development
 // here and there
 const isDev = false;
 const location = ref();
-const isInfoWindowVisible = ref(false);
-
-function filterView(viewType) {
-	return x => x.properties["Map Views"] === viewType 
-		&& x.properties["Approved for Map? (Internal Only)"] === "Approved";
-}
 
 const sources = reactive({
 	producers: {
@@ -158,8 +131,33 @@ const sources = reactive({
 	}
 });
 
-function hideInfoWindow() {
-	isInfoWindowVisible.value = false;
+const name = computed(() => prop("Business Name") || prop("Name"));
+const website = computed(() => prop("Business/Organization Website"));
+const address = computed(() => prop("Business/Organization Address"));
+
+const isMember = computed(() => prop("Membership Status") === "Active");
+
+const profileUrl = computed(() => prop("Link to PNWFC Member Profile"));
+const socialUrl = computed(() => prop("Link/s to social media"));
+const classUrl = computed(() => prop("Link to Classes"));
+
+const sell = computed(() => prop("What do they sell?"));
+const education = computed(() => prop("Educational Opportunities (Tours, courses, internships, etc.)"));
+
+const products = computed(() => parseList("Product and/or Service Type(s)"));
+const sourcing = computed(() => parseList("Product Sourcing Information"));
+
+const retailType = computed(() => parseList("Retailer Type"));
+const learningType = computed(() => prop("Learning Type"));
+const producerType = computed(() => parseList("Producer Type"));
+
+function locationSelected(x) {
+	location.value = x;
+}
+
+function filterView(viewType) {
+	return x => x.properties["Map Views"] === viewType 
+		&& x.properties["Approved for Map? (Internal Only)"] === "Approved";
 }
 
 function prop(propName) {
@@ -177,255 +175,4 @@ function isRetailer() {
 function parseList(x) {
 	return JSON.parse(prop(x) || "[]").join("; ");
 }
-
-const name = computed(() => prop("Business Name") || prop("Name"));
-// phone: computed(() => prop("Business Phone"))
-const phone = "(555) 753-1234";
-const website = computed(() => prop("Business/Organization Website"));
-const address = computed(() => prop("Business/Organization Address"));
-const products = computed(() => parseList("Product and/or Service Type(s)"));
-const sourcing = computed(() => parseList("Product Sourcing Information"));
-
-const isMember = computed(() => prop("Membership Status") === "Active");
-const profileUrl = computed(() => prop("Link to PNWFC Member Profile"));
-const socialUrl = computed(() => prop("Link/s to social media"));
-const sell = computed(() => prop("What do they sell?"));
-const education = computed(() => prop("Educational Opportunities (Tours, courses, internships, etc.)"));
-const classUrl = computed(() => prop("Link to Classes"));
-const retailType = computed(() => parseList("Retailer Type"));
-const learningType = computed(() => prop("Learning Type"));
-const producerType = computed(() => parseList("Producer Type"));
-
-
-onMounted(() => {
-	// Mapbox stuff
-	const map = new mapboxgl.Map({
-		container: "map",
-		style: "mapbox://styles/mapbox/streets-v12",
-		center: [-122.8, 47.04], // overriden below via setCenter
-		zoom: isDev ? 10 : 5,
-		accessToken:
-			"pk.eyJ1IjoicG53ZmMiLCJhIjoiY21kdWdxMWdxMGp4MjJrb2tydG5pNDBubCJ9.f6fYE5chIPEXv9Qx4P1VHg"
-	});
-
-	map.addControl(
-		new mapboxgl.NavigationControl({ showCompass: true }),
-		"top-left"
-	);
-
-	map.on("style.load", async () => {
-		const data = await getData();
-		const geoJson = toGeoJson(data);
-
-		const center = getCenter(data);
-		if (!isDev) {
-			map.setCenter(center);
-		}
-
-		// For development, start with a feature already selected
-		if (isDev) {
-			selectFeature({ feature: geoJson.features[25] });
-		}
-
-		Object.values(sources).forEach(source => {
-			map.addSource(source.id, { type: "geojson", data: {
-				...geoJson,
-				features: geoJson.features.filter(source.filter)
-			}})
-		});
-
-		Object.values(sources).forEach(source => {
-			map.addLayer(getIconLayer(source.id, source.icon));
-			map.addLayer(getTextLayer(source.id));
-		});
-
-		function getIconLayer(source, image) {
-			return {
-				id: `${source}-icons`,
-				type: "symbol",
-				source: source,
-				layout: {
-					"icon-image": image,
-					"icon-size": 1.2,
-					"icon-anchor": "top",
-					"icon-allow-overlap": true
-				}
-			};
-		}
-
-		function getTextLayer(source) {
-			return {
-				id: `${source}-names`,
-				type: "symbol",
-				source: source,
-				minzoom: 10, // text only appears at zoom 10+
-				layout: {
-					"text-field": "{Name}",
-					"text-size": 16,
-					"text-offset": [0, 0],
-					"text-anchor": "bottom",
-					"icon-image": "",
-					"icon-optional": true
-				},
-				paint: {
-					"text-color": "black",
-					"text-halo-color": whenFeatureState({
-						prop: "selected",
-						whenTrue: "pink",
-						whenFalse: "white"
-					}),
-					"text-halo-width": 1.5
-				}
-			};
-		}
-
-		Object.values(sources)
-			// each source has two layers: icons and text
-			.flatMap((source) => [`${source.id}-icons`, `${source.id}-names`])
-			.forEach((layer) => {
-				// Clicking on a feature will highlight it and display
-				// its properties in the info window
-				map.addInteraction(`click-${layer}`, {
-					type: "click",
-					target: { layerId: layer },
-					handler: selectFeature
-				});
-			
-				// Hovering over a feature will highlight it
-				map.addInteraction(`mouseenter-${layer}`, {
-					type: "mouseenter",
-					target: { layerId: layer },
-					handler: ({ feature }) => {
-						map.setFeatureState(feature, { highlight: true });
-						map.getCanvas().style.cursor = "pointer";
-					}
-				});
-
-				// Moving the mouse away from a feature will remove the highlight
-				map.addInteraction(`mouseleave-${layer}`, {
-					type: "mouseleave",
-					target: { layerId: layer },
-					handler: ({ feature }) => {
-						map.setFeatureState(feature, { highlight: false });
-						map.getCanvas().style.cursor = "";
-						return false;
-					}
-				});
-			});
-
-		function selectFeature({ feature }) {
-			if (location.value) {
-				map.setFeatureState(location.value, { selected: false });
-			}
-
-			location.value = feature;
-			isInfoWindowVisible.value = true;
-			map.setFeatureState(feature, { selected: true });
-		}
-
-		// React to changes in the selected sources
-		watchEffect(() => {
-			function getVisibility(b) {
-				return b ? "visible" : "none";
-			}
-
-			Object.values(sources).forEach(x => {			
-				map.setLayoutProperty(
-					`${x.id}-names`,
-					"visibility",
-					getVisibility(x.isVisible)
-				);
-
-				map.setLayoutProperty(
-					`${x.id}-icons`,
-					"visibility",
-					getVisibility(x.isVisible)
-				);			
-			});
-		});
-
-		// Clicking on the map will deselect the selected feature
-		map.addInteraction("map-click", {
-			type: "click",
-			handler: hideInfoWindow
-		});
-	});
-
-	function clearLocation() {
-		if (location.value) {
-			map.setFeatureState(location.value, { selected: false });
-			location.value = null;
-			isInfoWindowVisible.value = false;
-		}
-	}
-
-	// utility function to make MapboxGL easier to read
-	function whenFeatureState({ prop, whenTrue, whenFalse }) {
-		return [
-			"case",
-			["boolean", ["feature-state", prop], false],
-			whenTrue,
-			whenFalse
-		];
-	}
-
-	function toGeoJson(records) {
-		return {
-			type: "FeatureCollection",
-			features: records.map((x, index) => ({
-				id: index,
-				type: "Feature",
-				geometry: {
-					type: "Point",
-					coordinates: [parseFloat(x.Longitude), parseFloat(x.Latitude)]
-				},
-				properties: { ...x }
-			}))
-		};
-	}
-
-	// get the center of all the data points
-	function getCenter(data) {
-		const bounds = data.reduce(
-			(result, val) => {
-				const lng = parseFloat(val.Longitude);
-				const lat = parseFloat(val.Latitude);
-
-				if (lat < result.lat) {
-					result.lat = lat;
-				}
-				if (lng < result.lng) {
-					result.lng = lng;
-				}
-				if (lat > result.lat2) {
-					result.lat2 = lat;
-				}
-				if (lng > result.lng2) {
-					result.lng2 = lng;
-				}
-
-				return result;
-			},
-			{ lat: 90, lng: 0, lat2: 0, lng2: -180 }
-		);
-
-		const center = [
-			(bounds.lng + bounds.lng2) / 2.0,
-			(bounds.lat + bounds.lat2) / 2.0
-		];
-
-		return center;
-	}
-
-	async function getData() {
-		const res = await fetch(
-			"https://pnwfc-api-vercel.vercel.app/api/locations"
-		);
-		if (!res.ok) {
-			throw new Error(`Request failed: ${res.status}`);
-		}
-		return await res.json(); // parse JSON body
-	}
-
-});
 </script>
