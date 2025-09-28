faf8c55f3a4521b0f7a35aee8e8f2b4fc0e139f1
mapward <mapward@users.noreply.github.com>
2025-09-27 20:57:34 -0700

    Add basic vue app

---
+++ src/app.vue
@@ -0,0 +1,9 @@
+<template>
+	<div>Hello {{ world }}</div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const world = ref('World');
+</script>
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue';
+import WoolMapApp from './app.vue';
+
+createApp(WoolMapApp).mount('#wool-map-app');
